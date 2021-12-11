8c954fbc8771b49ad6f393a0cb549ebd3d840a30
pauhoms <47574509+pauhoms@users.noreply.github.com>
2021-12-11 14:48:45 -0800

    fix: naming

--- apps/configuration/database.php
+++ apps/configuration/database.php
@@ -8,21 +8,21 @@ use Doctrine\DBAL\Types\Type;
 return function (): EntityManager {
     $settings = require __DIR__ . '/settings.php';
     $config = Setup::createXMLMetadataConfiguration(
-        $settings['Authentication-mysql']['metadata_dirs'],
+        $settings['-mysql']['metadata_dirs'],
         true
     );
 
-    $driver = new XmlDriver($settings['Authentication-mysql']['metadata_dirs']);
+    $driver = new XmlDriver($settings['mysql']['metadata_dirs']);
     $config->setMetadataDriverImpl($driver);
 
-    foreach ($settings['Authentication-mysql']['custom-type'] as $type) {
+    foreach ($settings['mysql']['custom-type'] as $type) {
         if (!Type::hasType($type[0])) {
             Type::addType($type[0], $type[1]);
         }
     }
 
     return EntityManager::create(
-        $settings['Authentication-mysql']['connection'],
+        $settings['mysql']['connection'],
         $config
     );
 };
