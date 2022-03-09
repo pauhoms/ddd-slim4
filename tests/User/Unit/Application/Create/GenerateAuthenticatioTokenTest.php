c772a70c358f9486e8c0f3e2ee85f4ee34841c26
pauhoms <pauhomsjimenez1@gmail.com>
2022-03-09 20:36:47 +0100

    feat: improve user tests

--- tests/User/Unit/Application/Create/GenerateAuthenticatioTokenTest.php
+++ tests/User/Unit/Application/Create/GenerateAuthenticatioTokenTest.php
@@ -5,6 +5,10 @@ namespace Tests\User\Unit\Application\Create;
 
 use PHPUnit\Framework\TestCase;
 use User\Application\Create\GenerateAuthenticationToken;
+use User\Domain\Exceptions\PasswordDoesNotMatch;
+use User\Domain\Exceptions\UserNotFound;
+use User\Domain\Repositories\TokenRepository;
+use User\Domain\Repositories\UserRepository;
 use User\Domain\Services\FindUserByName;
 use User\Domain\User;
 use User\Domain\ValueObjects\UserId;
@@ -14,14 +18,21 @@ use User\Domain\ValueObjects\UserPrivateKey;
 use User\Infrastructure\Persistence\Fake\FakeTokenRepository;
 use User\Infrastructure\Persistence\Fake\FakeUserRepository;
 
-final class UserPrivateKeyTest extends TestCase
+final class GenerateAuthenticationTokenTest extends TestCase
 {
+    private TokenRepository $tokenRepository;
+    private UserRepository  $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->tokenRepository = new FakeTokenRepository();
+        $this->userRepository  = new FakeUserRepository();
+    }
+
     /** @test */
     public function token_should_be_generated(): void
     {
-        $tokenRepository = new FakeTokenRepository();
-        $userRepository = new FakeUserRepository();
-        $userRepository->save(
+        $this->userRepository->save(
             new User(
                 UserId::random(),
                 new UserName("esther"),
@@ -30,15 +41,49 @@ final class UserPrivateKeyTest extends TestCase
             )
         );
 
-        $userService = new FindUserByName($userRepository);
-        $generateToken = new GenerateAuthenticationToken(
-            $tokenRepository,
+        $userService = new FindUserByName($this->userRepository);
+        $service = new GenerateAuthenticationToken(
+            $this->tokenRepository,
             $userService
         );
 
         $this->assertNotNull(
-            $generateToken->__invoke("esther", "garcia")
+            $service->__invoke("esther", "garcia")
         );
+    }
+
+    /** @test */
+    public function user_should_not_exist(): void
+    {
+        $userService = new FindUserByName($this->userRepository);
+        $service = new GenerateAuthenticationToken(
+            $this->tokenRepository,
+            $userService
+        );
+
+        $this->expectException(UserNotFound::class);
+        $service->__invoke("test", "garcia");
+    }
+
+    /** @test */
+    public function password_should_not_be_match(): void
+    {
+        $userService = new FindUserByName($this->userRepository);
+        $this->userRepository->save(
+            new User(
+                UserId::random(),
+                new UserName("esther"),
+                UserPassword::create("garcia"),
+                UserPrivateKey::generate()
+            )
+        );
+        $service = new GenerateAuthenticationToken(
+            $this->tokenRepository,
+            $userService
+        );
+
+        $this->expectException(PasswordDoesNotMatch::class);
+        $service->__invoke("esther", "homs");
     }
 }
 
